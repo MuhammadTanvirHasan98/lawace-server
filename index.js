c9219ef26d2a541d475ed415705c992d8052023e
MuhammadTanvirHasan98 <muhammadtanvirhasan98@gmail.com>
2025-01-05 12:34:38 +0600

    "Blogs,Users, Lawyers and other necessary API has been done"

--- index.js
+++ index.js
@@ -24,66 +24,17 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
     const db = client.db("lawaceDB");
 
-    const allMealCollection = db.collection("allMeals");
+    const allBlogCollection = db.collection("blogs");
     const userCollection = db.collection("users");
+    const lawyerCollection = db.collection("lawyers");
+    const allCommentCollection = db.collection("comments");
     const requestedMealCollection = db.collection("requestedMeals");
     const reviewCollection = db.collection("reviews");
     const userPackageCollection = db.collection("userPackages");
 
-    // ---------- Stripe Payment related API ---------- //
-    // app.post("/create-payment-intent", async (req, res) => {
-    //   const { price } = req.body;
-    //   const priceInCent = parseFloat(price) * 100;
-
-    //   if (!price || priceInCent < 1) return;
-
-    //   // generate clientSecret
-    //   const { client_secret } = await stripe.paymentIntents.create({
-    //     amount: priceInCent,
-    //     currency: "usd",
-    //     // In the latest version of the API, specifying the `automatic_payment_methods` parameter is optional because Stripe enables its functionality by default.
-    //     automatic_payment_methods: {
-    //       enabled: true,
-    //     },
-    //   });
-
-    //   // Send client secret as response
-    //   res.send({ clientSecret: client_secret });
-    // });
-
-    // //  all purchase history of specific user
-    // app.get("/paymentHistory/:email", async (req, res) => {
-    //   const email = req.params.email;
-    //   console.log(email);
-    //   const result = await userPackageCollection
-    //     .find({ user_email: email })
-    //     .toArray();
-    //   res.send(result);
-    // });
-
-    // //  User purchasing packages save to database
-    // app.post("/purchasePackage", async (req, res) => {
-    //   const packageData = req.body;
-    //   console.log(packageData);
-
-    //   const query = {
-    //     email: packageData?.user_email,
-    //   };
-    //   const updatedDoc = {
-    //     $set: {
-    //       badge: packageData?.plan,
-    //     },
-    //   };
-
-    //   await userCollection.updateOne(query, updatedDoc);
-
-    //   const result = await userPackageCollection.insertOne(packageData);
-    //   res.send(result);
-    // });
-
+    // ********** Users Related API *********//
     // get a user info by email from db
     app.get("/user/:email", async (req, res) => {
       const email = req.params.email;
@@ -91,11 +42,122 @@ async function run() {
       res.send(result);
     });
 
+    // Add user to database when user is created in the client site
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      console.log(user);
+
+      // check whether user exists or not. Insert user only if user does not exist
+      const query = { email: user.email };
+      const existUser = await userCollection.findOne(query);
+      console.log("Check user is present or not:", existUser);
+      if (existUser) {
+        return res.send({ message: "User already exists!", insertedId: null });
+      }
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    });
+
+    // ********** Users API  Ends here*********//
+
+    // ********** Lawyers Related API *********//
+    // get all lawyers info from database
+    app.get("/lawyers", async (req, res) => {
+      const result = await lawyerCollection.find().toArray();
+      res.send(result);
+    });
+
+    // get a lawyer info by email from database
+    app.get("/lawyer/:email", async (req, res) => {
+      const lawyer_email = req.params.email;
+      const result = await lawyerCollection.findOne({ lawyer_email });
+      res.send(result);
+    });
+
+    // Add or Update new or existing lawyer to the database
+    app.put("/lawyer/:email?", async (req, res) => {
+      const email = req.params.email;
+      const lawyerProfile = req.body;
+      console.log("Lawyer Info:", { lawyerProfile, email });
+      const query = { lawyer_email: lawyerProfile?.lawyer_email };
+
+      const options = { upsert: true }; // Create if it doesn't exist
+      const updateDoc = { $set: { ...lawyerProfile } };
+      const result = await lawyerCollection.updateOne(
+        query,
+        updateDoc,
+        options
+      );
+      res.send(result);
+    });
+
+    // ********** Lawyer API  Ends here*********//
+
+    //*************** Blogs API **************//
+
+    // ******* Fetch All blogs from the DB ******//
+    app.get("/blogs", async (req, res) => {
+      const search = req.query.search;
+      let query = {};
+      if (search) {
+        query.blog_title = { $regex: search, $options: "i" };
+      }
+      const result = await allBlogCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    // get single blog data from database
+    app.get("/blog/:id", async (req, res) => {
+      const id = req.params.id;
+      console.log("Get blog id:", id);
+      const query = { _id: new ObjectId(id) };
+      const result = await allBlogCollection.findOne(query);
+      res.send(result);
+    });
+
+    // Save a blog by user and lawyer to the database
+    app.post("/blog", async (req, res) => {
+      const blogInfo = req.body;
+      console.log(blogInfo);
+      const result = await allBlogCollection.insertOne(blogInfo);
+      res.send(result);
+    });
+
+    // to update likes count of meal data in the database
+    app.patch("/blog/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const options = {
+        $inc: { like: 1 },
+      };
+      const result = await allBlogCollection.updateOne(query, options);
+      res.send(result);
+    });
+    //****** Blogs API Ends here *******//
+
+    // ---------- Comments related API ---------- //
+    // get all comments for specific blog from database
+    app.get("/comments/:id", async (req, res) => {
+      const id = req.params.id;
+      console.log("Comment blog id:", id);
+      const query = { blogId: id };
+      const result = await allCommentCollection.find(query).toArray();
+      res.send(result);
+    });
+    // Save a comment by user and lawyer to the database
+    app.post("/comment", async (req, res) => {
+      const commentInfo = req.body;
+      console.log(commentInfo);
+      const result = await allCommentCollection.insertOne(commentInfo);
+      res.send(result);
+    });
+    // ---------- Comments API Ends here ---------- //
+
     // ---------- Meals related API ---------- //
 
     // get all meals for admin dashboard from database
     app.get("/dasAllMeals", async (req, res) => {
-      const result = await allMealCollection.find().toArray();
+      const result = await allBlogCollection.find().toArray();
       res.send(result);
     });
 
@@ -116,7 +178,7 @@ async function run() {
         const [minPrice, maxPrice] = price.split("-").map(Number);
         query.price = { $gte: minPrice, $lte: maxPrice };
       }
-      const result = await allMealCollection.find(query).toArray();
+      const result = await allBlogCollection.find(query).toArray();
       if (result.length === 0) {
         return res.status(204).send();
       }
@@ -127,7 +189,7 @@ async function run() {
     app.get("/meal/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
-      const result = await allMealCollection.findOne(query);
+      const result = await allBlogCollection.findOne(query);
       res.send(result);
     });
 
@@ -144,14 +206,6 @@ async function run() {
       const result = await requestedMealCollection
         .find({ "userInfo.email": email })
         .toArray();
-      res.send(result);
-    });
-
-    // Save a meal by admin to the database
-    app.post("/addMeal", async (req, res) => {
-      const mealData = req.body;
-      console.log(mealData);
-      const result = await allMealCollection.insertOne(mealData);
       res.send(result);
     });
 
@@ -182,7 +236,7 @@ async function run() {
       const options = {
         $inc: { likes: 1 },
       };
-      const result = await allMealCollection.updateOne(query, options);
+      const result = await allBlogCollection.updateOne(query, options);
       res.send(result);
     });
 
@@ -191,7 +245,7 @@ async function run() {
       const id = req.params.id;
       console.log(id);
       const query = { _id: new ObjectId(id) };
-      const result = await allMealCollection.deleteOne(query);
+      const result = await allBlogCollection.deleteOne(query);
       res.send(result);
     });
 
@@ -223,37 +277,6 @@ async function run() {
         admin = user.role === "admin";
       }
       res.send({ admin });
-    });
-
-    // Get all users for admin route from database
-    app.get("/users", async (req, res) => {
-      const result = await userCollection.find().toArray();
-      res.send(result);
-    });
-
-    // Get specific user by email for user or admin profile info
-    app.get("/users/:email", async (req, res) => {
-      const email = req.params.email;
-      console.log(email);
-      const query = { email: email };
-      const result = await userCollection.findOne(query);
-      res.send(result);
-    });
-
-    // Add user to database when user is created in the client site
-    app.post("/users", async (req, res) => {
-      const user = req.body;
-      console.log(user);
-
-      // check whether user exists or not. Insert user only if user does not exist
-      const query = { email: user.email };
-      const existUser = await userCollection.findOne(query);
-      console.log(existUser);
-      if (existUser) {
-        return res.send({ message: "User already exists!", insertedId: null });
-      }
-      const result = await userCollection.insertOne(user);
-      res.send(result);
     });
 
     // to update likes count of meal data in the database
@@ -306,7 +329,7 @@ async function run() {
       };
       const result = await reviewCollection.insertOne(review);
       if (result.insertedId) {
-        await allMealCollection.updateOne(query, options);
+        await allBlogCollection.updateOne(query, options);
       }
       res.send(result);
     });
@@ -339,7 +362,7 @@ async function run() {
       const options = {
         $inc: { reviews: -1 },
       };
-      await allMealCollection.updateOne(filter, options);
+      await allBlogCollection.updateOne(filter, options);
       const result = await reviewCollection.deleteOne(query);
       res.send(result);
     });
